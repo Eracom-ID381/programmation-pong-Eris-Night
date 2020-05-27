60be3d152abc7086f60d3fa149fff8da564f509f
Eris-Night <Eris-Night@users.noreply.github.com>
2020-05-27 15:52:34 +0200

    reconstruction js qui s'affiche totalement

--- js/sketch.js
+++ js/sketch.js
@@ -1,113 +1,120 @@
 let scoreLeft = 0;
 let scoreRight = 0;
 
-let ball = {
-    x: 0,
-    y: 0,
-    speedX: 10,
-    speedY: 0,
-    radius: 40
-}
-
-let paddleLeft = {
-    x: 30,
-    y: 0,
-    width: 20,
-    height: 150
-}
-
-let paddleRight = {
-    x: 0,
-    y: 0,
-    width: 20,
-    height: 150
-}
-
+let balle;
+let paddleLeft;
+let paddleRight;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     rectMode(CENTER);
     noStroke();
 
-    ball.x = width / 2;
-    ball.y = height / 2;
-
-    paddleRight.x = width - 30;
+    balle = new Balle(width / 2, height / 2);
+    paddleLeft = new PaddleLeft();
+    paddleRight = new PaddleRight(width - 30);
 }
 
 function draw() {
     background(0);
     fill(255);
-    moveBall();
-    bounceBall();
     drawElements();
+    balle.afficher();
+    paddleLeft.afficher();
+    paddleRight.afficher();
+    balle.moveBall();
+    balle.bounceBall();
 }
 
 function drawElements() {
-    paddleRight.y = mouseY;
-    paddleLeft.y = mouseX;
-    rect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
-    rect(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height);
-    ellipse(ball.x, ball.y, ball.radius);
     textSize(100);
     textAlign(RIGHT)
     text(scoreLeft, width / 2 - 40, 100);
     textAlign(LEFT)
 
-    text(scoreRight, width/2 + 40 , 100);
+    text(scoreRight, width / 2 + 40, 100);
 
     for (let y = 0; y < height; y = y + 30) {
         rect(width / 2, y, 20, 20);
     }
 }
 
-function bounceBall() {
-    // Detection de collision Paddle Right
-    if (ball.x >= paddleRight.x - paddleRight.width * 2 &&
-        ball.y >= paddleRight.y - paddleRight.height / 2 &&
-        ball.y <= paddleRight.y + paddleRight.height / 2) {
-        ball.speedX = -ball.speedX;
-        ball.speedY = random(-5, 5);
+class Balle {
+    constructor(_x, _y) {
+        this.x = _x;
+        this.y = _y;
+        this.speedX = 10;
+        this.speedY = 0;
+        this.radius = 40;
     }
-
-    // Detection de collision Paddle Left 
-    if (ball.x <= paddleLeft.x + paddleLeft.width * 2 &&
-        ball.y >= paddleLeft.y - paddleLeft.height / 2 &&
-        ball.y <= paddleLeft.y + paddleLeft.height / 2) {
-        ball.speedX = -ball.speedX;
-        ball.speedY = random(-5, 5);
+    afficher() {
+        ellipse(this.x, this.y, this.radius);
     }
-
-    // Detection collision "murs" haut et bas
-    if (ball.y <= ball.radius || ball.y >= height - ball.radius) {
-        ball.speedY = -ball.speedY;
+    moveBall() {
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
+    bounceBall() {
+        // Detection de collision Paddle Right
+        if (this.x >= paddleRight.x - paddleRight.width * 2 &&
+            this.y >= paddleRight.y - paddleRight.height / 2 &&
+            this.y <= paddleRight.y + paddleRight.height / 2) {
+            this.speedX = -this.speedX;
+            this.speedY = random(-5, 5);
+        }
 
-    if (ball.x > width) {
-        resetBall('left');
-        scoreLeft += 1;
-    } else if (ball.x < 0) {
-        resetBall('right');
-        scoreRight += 1;
+        // Detection de collision Paddle Left
+        if (this.x <= paddleLeft.x + paddleLeft.width * 2 &&
+            this.y >= paddleLeft.y - paddleLeft.height / 2 &&
+            this.y <= paddleLeft.y + paddleLeft.height / 2) {
+            this.speedX = -this.speedX;
+            this.speedY = random(-5, 5);
+        }
+
+        // Detection collision "murs" haut et bas
+        if (this.y <= this.radius || this.y >= height - this.radius) {
+            this.speedY = -this.speedY;
+        }
+
+        if (this.x > width) {
+            resetBall('left');
+            scoreLeft += 1;
+        } else if (this.x < 0) {
+            resetBall('right');
+            scoreRight += 1;
+        }
+    }
+    resetBall() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.speedX = -this.speedX;
+        this.speedY = random(-2, 2);
+
     }
 }
 
-function movePaddle() {
-    paddleRight.y = mouseY;
-    paddleLeft.y = mouseX;
+class PaddleLeft {
+    constructor() {
+        this.x = 30;
+        this.y = 0;
+        this.width = 20;
+        this.height = 150;
+    }
+    afficher() {
+        rect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
+    }
 }
 
-function moveBall() {
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
-
-function resetBall() {
-    ball.x = width / 2;
-    ball.y = height / 2;
-    ball.speedX = -ball.speedX;
-    ball.speedY = random(-2, 2);
-
+class PaddleRight {
+    constructor(_x) {
+        this.x = _x;
+        this.y = 0;
+        this.width = 20;
+        this.height = 150;
+    }
+    afficher() {
+        rect(this.x, this.y, this.width, this.height);
+    }
 }
 
 function windowResized() {
