1a9e8b5f56fc687cc518a1bba32d2155fec0f5cb
Eris-Night <Eris-Night@users.noreply.github.com>
2020-06-03 18:58:32 +0200

    colors et vitesse

--- js/sketch.js
+++ js/sketch.js
@@ -4,6 +4,7 @@ let scoreRight = 0;
 let balle;
 let paddleLeft;
 let paddleRight;
+var tamère = true;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
@@ -16,7 +17,7 @@ function setup() {
 }
 
 function draw() {
-    background(0);
+    background(color(random(0, 255), random(0, 255), random(0, 255)));
     fill(255);
     drawElements();
     balle.afficher();
@@ -28,6 +29,8 @@ function draw() {
 
     balle.moveBall();
     balle.bounceBall();
+
+
 }
 
 function drawElements() {
@@ -47,7 +50,7 @@ class Balle {
     constructor(_x, _y) {
         this.x = _x;
         this.y = _y;
-        this.speedX = 10;
+        this.speedX = 70;
         this.speedY = 0;
         this.radius = 40;
     }
@@ -65,6 +68,7 @@ class Balle {
             this.y <= paddleRight.y + paddleRight.height / 2) {
             this.speedX = -this.speedX;
             this.speedY = random(-5, 5);
+            scoreRight += 1;
         }
 
         // Detection de collision Paddle Left
@@ -73,6 +77,7 @@ class Balle {
             this.y <= paddleLeft.y + paddleLeft.height / 2) {
             this.speedX = -this.speedX;
             this.speedY = random(-5, 5);
+            scoreLeft += 1;
         }
 
         // Detection collision "murs" haut et bas
@@ -82,10 +87,10 @@ class Balle {
 
         if (this.x > width) {
             resetBall('left');
-            scoreLeft += 1;
+            scoreLeft -= 1;
         } else if (this.x < 0) {
             resetBall('right');
-            scoreRight += 1;
+            scoreRight -= 1;
         }
     }
 }
